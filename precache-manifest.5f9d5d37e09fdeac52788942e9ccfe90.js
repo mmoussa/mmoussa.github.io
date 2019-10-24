f466bc1bf2400b3ae0947ed2af10076634f928a1
mmoussa <mmoussa1994@gmail.com>
2019-10-23 23:51:47 -0400

    Updates

---
+++ precache-manifest.5f9d5d37e09fdeac52788942e9ccfe90.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2120bebdef7cb00cb7919e1968886df0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "edf3d9963bb7a2f0ed0a",
+    "url": "/static/css/2.60a6b8e0.chunk.css"
+  },
+  {
+    "revision": "dbf76f5da5a3d45f34cb",
+    "url": "/static/css/main.502dc7f7.chunk.css"
+  },
+  {
+    "revision": "edf3d9963bb7a2f0ed0a",
+    "url": "/static/js/2.99ae9574.chunk.js"
+  },
+  {
+    "revision": "dbf76f5da5a3d45f34cb",
+    "url": "/static/js/main.5417d6e5.chunk.js"
+  },
+  {
+    "revision": "c2e40496bf4542af17dd",
+    "url": "/static/js/runtime~main.7fe8d152.js"
+  },
+  {
+    "revision": "3c7c0eeea21f31dd7ecf117098fc57e9",
+    "url": "/static/media/IMG_5915.3c7c0eee.JPG"
+  },
+  {
+    "revision": "7ad72f3fab773b81eb37b9959ba6002d",
+    "url": "/static/media/ic_travelar.7ad72f3f.svg"
+  }
+]);
