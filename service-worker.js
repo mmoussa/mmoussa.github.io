da1b9e4442f14916ee059a1de9159fd4a92adb42
mmoussa <mmoussa1994@gmail.com>
2019-10-24 00:48:28 -0400

    update2

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.56a34912e6bbfd109040d6c4b1b03aa2.js"
+  "/precache-manifest.a5acaceb7eba54491f58c508ae0601f9.js"
 );
 
 self.addEventListener('message', (event) => {
